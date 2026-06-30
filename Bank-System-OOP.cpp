959025f14ad2f630de898755ba0ebd2e0d342d4b
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-06-30 14:57:31 +0300

    Add clsBankClient class and update client handling logic
    Refactored `Bank-System-OOP.cpp` to demonstrate the new `clsBankClient` class functionality, including client search and data printing. Removed unused includes and added necessary headers.
    
    Replaced `Clients.txt` data with updated client records because the order in which data is displayed affects how the program works. and added `Clients2.txt` as a backup of the original data.
    Implemented `clsBankClient` class in `clsBankClient.h`:
    - Added private members for account details and an `enMode` enum.
    - Implemented methods for client search, data access, and printing.
    - Added `_ConvertLineToClientObject` for parsing client data from files.

--- Bank-System-OOP.cpp
+++ Bank-System-OOP.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
-#include "clsPeriod.h"
-#include "clsString.h"
-#include "clsUtil.h"
-#include "clsDate.h"
-#include "clsInputValidate.h"
+#include "clsBankClient.h"
+#include <cstdlib>
 
 using namespace std;
 
 int main()
+
 {
 
-}
+    clsBankClient Client1 = clsBankClient::Find("A101");
+    if (!Client1.IsEmpty())
+    {
+        cout << "\nClient Found ^_^\n";
+    }
+    else
+    {
+        cout << "\nClient Was not Found :(\n";
+    }
 
+    Client1.Print();
+
+    /*clsBankClient Client2 = clsBankClient::Find("A101", "1234");
+    if (!Client2.IsEmpty())
+    {
+        cout << "\nClient Found :-)\n";
+    }
+    else
+    {
+        cout << "\nClient Was Not Found :-(\n";
+    }
+    Client2.Print();*/
+
+
+  /*  cout << "\nIs Client Exist? " << clsBankClient::IsClientExist("A101");*/
+    
+    system("pause>0");
+    return 0;
+}
---
+++ clsBankClient.h
@@ -0,0 +1,114 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "clsPerson.h"
+#include "clsString.h"
+#include <vector>
+#include <fstream>
+
+using namespace std;
+class clsBankClient : public clsPerson
+{
+private:
+	enum enMode { EmptyMode = 0, UpdateMode = 1 };
+	enMode _Mode;
+
+	string _AccountNumber;
+	string _PinCode;
+	float _AccountBalance;
+
+	/*const string _ClientsFileName = "Clients.txt";
+	const string _UsersFileName = "Users.txt";*/
+
+
+	/*static string _ConvertClientObjectToLine(const clsBankClient& Client, string Separator = "#//#")
+	{
+		string ClientObject = "";
+		ClientObject += Client.AccountNumber + Separator;
+		ClientObject += Client.Name + Separator;
+		ClientObject += Client.Phone + Separator;
+		ClientObject += Client.PinCode + Separator;
+		ClientObject += to_string(Client.AccountBalance);
+
+		return ClientObject;
+	}*/
+
+
+	static clsBankClient _ConvertLineToClientObject(string Line, string Separator = "#//#")
+	{
+		vector<string> vClientData = clsString::Split(Line, Separator);
+
+		return clsBankClient(enMode::UpdateMode, vClientData[0], vClientData[1], vClientData[2], vClientData[3], vClientData[4], vClientData[5], stof(vClientData[6]));
+	}
+
+
+
+public:
+
+	clsBankClient(enMode Mode, string FirstName, string LastName,
+		string Email, string Phone, string AccountNumber, string PinCode,
+		float AccountBalance) :
+		clsPerson(FirstName, LastName, Email, Phone)
+	{
+		_Mode = Mode;
+		_AccountNumber = AccountNumber;
+		_PinCode = PinCode;
+		_AccountBalance = AccountBalance;
+	}
+
+	bool IsEmpty()
+	{
+		return (_Mode == enMode::EmptyMode);
+	}
+
+	string AccountNumber() { return _AccountNumber; } // Get only
+
+	void SetPinCode(string PinCode) { _PinCode = PinCode; }
+	string GetPinCode() { return _PinCode; }
+	__declspec(property(get = GetPinCode, put = SetPinCode)) string PinCode;
+
+	void SetAccountBalance(float AccountBalance) { _AccountBalance = AccountBalance; };
+	float GetAccountBalance() { return _AccountBalance; }
+	__declspec(property(get = GetAccountBalance, put = SetAccountBalance)) float AccountBalance;
+
+	void Print()
+	{
+		cout << "\nClient Card:";
+		cout << "\n___________________";
+		cout << "\nFirstName   : " << FirstName;
+		cout << "\nLastName    : " << LastName;
+		cout << "\nFull Name   : " << FullName();
+		cout << "\nEmail       : " << Email;
+		cout << "\nPhone       : " << Phone;
+		cout << "\nAcc. Number : " << _AccountNumber;
+		cout << "\nPassword    : " << _PinCode;
+		cout << "\nBalance     : " << _AccountBalance;
+		cout << "\n___________________\n";
+
+	}
+
+
+	static clsBankClient Find(const string& AccountNumber)
+	{
+		vector<clsBankClient> vClients;
+
+		fstream myFile;
+		myFile.open("Clients.txt", ios::in); //Read mode
+
+		if (myFile.is_open())
+		{
+			string line;
+			while (getline(myFile, line))
+			{
+				clsBankClient client = _ConvertLineToClientObject(line);
+				if (client.AccountNumber() == AccountNumber)
+				{
+					myFile.close();
+					return client;
+				}
+				vClients.push_back(client);
+			}
+		}
+
+	}
+};
