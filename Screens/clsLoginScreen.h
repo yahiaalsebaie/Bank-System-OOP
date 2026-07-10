b27b881fdfd9855c44c5de3ee0528ee8908d40ac
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-07-10 22:37:55 +0300

    Add login record logging and improve user feedback
    Added `_PrepareLoginRecord` and `_RegisterLogIn` methods to log user login events in `LoginRegister.txt`. Updated `_Login` to call `_RegisterLogIn` after successful login. Corrected variable name `Trails` to `Trials` and improved user feedback for remaining login attempts. Included new headers for date/time handling and file operations. Modified `LoginRegister.txt` to include sample login records.

--- Screens/clsLoginScreen.h
+++ Screens/clsLoginScreen.h
@@ -3,8 +3,10 @@
 #include "clsScreen.h";
 #include "clsUser.h";
 #include "Global.h";
+#include <clsDate.h>
 #include <clsUtil.h>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -12,13 +14,44 @@
 class clsLoginScreen : private clsScreen
 {
 private:
+
+	static string _PrepareLoginRecord(const clsUser& User, string Separator = "#//#")
+	{
+
+		string DateTime = clsDate::GetSystemDateTimeString();
+		string Record = "";
+
+		Record += DateTime + Separator;
+		Record += User.UserName() + Separator;
+		Record += User.Password + Separator; //Just for learning purposes
+		Record += to_string(User.Permissions);
+
+		return Record;
+	}
+
+	static void _RegisterLogIn()
+	{
+		string RecordLine = _PrepareLoginRecord(CurrentUser);
+
+		fstream myFile;
+		myFile.open("LoginRegister.txt", ios::out | ios::app);
+
+		if (myFile.is_open())
+		{
+			myFile << RecordLine << endl;
+			myFile.close();
+		}
+	}
+
+
+
 	static bool _Login()
 	{
 		bool loginFailed = false;
 
 		string userName = "", Password = "";
 
-		short Trails = 2;
+		short Trials = 2;
 
 		do
 		{
@@ -26,15 +59,15 @@ private:
 			{
 
 				cout << clsUtil::ColorText("\n Invalid UserName/Password!\n\n", clsUtil::enColor::BEIGE);
-				cout << " You have [" << clsUtil::ColorText(to_string(Trails), clsUtil::enColor::BRIGHT_YELLOW) << "] to login.\n\n";
+				cout << " You have [" << clsUtil::ColorText(to_string(Trials), clsUtil::enColor::BRIGHT_YELLOW) << "] trial(s) left.\n\n";
 
-				if (Trails == 0)
+				if (Trials == 0)
 				{
 					system("cls");
 					cout << clsUtil::ColorText("\n\n\n\n\n\n\n\n You are locked after 3 trail(s)\n\n", clsUtil::enColor::BEIGE);
 					return false;
 				}
-				Trails--;
+				Trials--;
 
 			}
 
@@ -50,7 +83,7 @@ private:
 
 
 		} while (loginFailed);
-
+		_RegisterLogIn();
 		clsMainScreen::ShowMainMenu();
 		return true;
 	}
