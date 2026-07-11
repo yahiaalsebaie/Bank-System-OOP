7e0ce19a506feea6009efccc1c8afc9f5b059cd7
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-07-11 20:55:47 +0300

    Add  Show Transfers Log Screen Add transfer logs functionality to banking system
    Introduced a new feature to log and display transfer records.
    Added `clsTransferLog.h` and `clsTransfersLogScreen.h` to
    handle transfer logs. Updated `clsTransactionsScreen.h` to
    include a "Transfer Logs" menu option.
    
    Enhanced `clsBankClient.h` with `stTransferLogRecord` and
    methods to prepare and retrieve transfer logs. Updated
    `TransferRegister.txt` with new transfer records and
    `Clients.txt` with modified balances.
    
    Refactored `clsUser.h` for cleaner user data handling and
    updated `LoginRegister.txt` with new login records. Removed unused methods in `clsLoginRegisterScreen.h`. Made minor UI
    adjustments to reflect the new functionality.

---
+++ Screens/Transactions/clsTransfersLogScreen.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "clsBankClient.h"
+#include "clsScreen.h"
+#include "clsUtil.h"
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+class clsTransfersLogScreen :protected clsScreen
+{
+private:
+
+
+	static void _PrintTransfersLogRecordLine(clsBankClient::stTransferLogRecord TransfersLogRecord)
+	{
+
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(25) << TransfersLogRecord.DateTime;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.SourceAccountNumber;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.DestinationBalanceAfter;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.TransferAmount;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.SourceBalanceAfter;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.DestinationBalanceAfter;
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << TransfersLogRecord.UserName;
+
+	}
+
+	
+public:
+
+	static void ShowTransfersLogsScreen()
+	{
+
+		vector <clsBankClient::stTransferLogRecord> stRecords = clsBankClient::GetTransferRegisterList();
+		string SeparatorLine = "\n_________________________________________________________________________________________________________\n";
+		string NumberOfRecords = to_string(stRecords.size());
+
+		string subTitle = clsUtil::ColorText("\t    [", clsUtil::enColor::RED) + clsUtil::ColorText(NumberOfRecords, clsUtil::enColor::YELLOW) + clsUtil::ColorText("]", clsUtil::enColor::RED) + " Record(s).";
+
+		_DrawScreenHeader("    Login Register List Screen", subTitle);
+
+		cout << clsUtil::ColorText(SeparatorLine, clsUtil::enColor::YELLOW);
+
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(25) << "Date/Time";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "src.Acc";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "des.Acc";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "Amount";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "src.Balance";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "des.Balance";
+		cout << clsUtil::ColorText("| ", clsUtil::enColor::RED) << left << setw(11) << "User";
+		cout << clsUtil::ColorText(SeparatorLine, clsUtil::enColor::YELLOW);
+
+		if (stRecords.size() == 0)
+			clsUtil::PrintColoredTextLine("\t\t\t\tNo Records Available In System!", clsUtil::enColor::RED);
+		else
+
+			for (const clsBankClient::stTransferLogRecord& Record : stRecords)
+			{
+				_PrintTransfersLogRecordLine(Record);
+				cout << endl;
+			}
+
+		cout << clsUtil::ColorText(SeparatorLine, clsUtil::enColor::YELLOW);
+
+	}
+
+
+};
+
